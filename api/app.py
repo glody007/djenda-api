999c3effb84a9491b36030381a5f76236e658a7a
glody007 <glodymbutwile@gmail.com>
2020-07-18 16:21:45 +0200

    test post produit

--- api/app.py
+++ api/app.py
@@ -224,12 +224,12 @@ def add_produit_only():
         user = User(unique_id = 1,
                     nom ="alchimiste",
                     email = "djendambutwile@gmail.com",
-                    photo = "https://res.cloudinary.com/alchemist118/image/upload/w_100,h_100/v1595080373/mario.jpg")
+                    url_photo = "https://res.cloudinary.com/alchemist118/image/upload/w_100,h_100/v1595080373/mario.jpg")
     produit = Produit(nom=request.json['nom'],
                       prix=request.json['prix'],
                       categorie=request.json['categorie'],
                       description=request.json['description'],
-                      url_photo=request.json['url_photo']).save()
+                      url_photo="https://res.cloudinary.com/alchemist118/image/upload/w_100,h_100/v1595080373/mario.jpg"]).save()
     user.produits.append(produit)
     user.save()
     return user.articles_to_json()
